9bbd5aa097c9558930a54940f5f080749c4e3f9d
ShubhamKThakur <88872926+ShubhamThakur088@users.noreply.github.com>
2026-01-27 18:52:36 +0530

    Data Cleaning using alter and drop command

--- chicago_city_crime_analysis.sql
+++ chicago_city_crime_analysis.sql
@@ -3,6 +3,9 @@ SELECT * FROM chicago_city_crime_incidents;
 --query to find the total number of crime records in the dataset.
 SELECT COUNT(*) FROM chicago_city_crime_incidents;
 
+--Data cleaning, i.e., removing redundant column location
+ALTER TABLE chicago_city_crime_incidents DROP COLUMN "LOCATION";
+
 --Finding the number of crimes by their primary_type ordered in decreasing order
 SELECT "PRIMARY_TYPE", COUNT("ID") AS incident_id FROM chicago_city_crime_incidents GROUP BY "PRIMARY_TYPE" ORDER BY incident_id;
 
